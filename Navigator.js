3ff92e51f14c5c61d19bd60f1acd25f14f5d267f
Aleksander Zhurbey <azhurbey@bellintegrator.com>
2020-02-12 22:38:48 +0400

    new navigation

--- Navigator.js
+++ Navigator.js
@@ -1,20 +1,20 @@
-//import {NavigationContainer} from '@react-navigation/native';
-//import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import {MonthScreen} from './containers/monthmode';
 import {YearScreen} from './containers/YearScreen';
 
-//const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
-//const RootStack = () => {
- // return (
-  //  <NavigationContainer>
-  //    <Stack.Navigator initialRouteName="Year">
-  //      <Stack.Screen name="Year" component={YearScreen} />
-  //      <Stack.Screen name="Month" component={MonthScreen} />
-  //    </Stack.Navigator>
-  //  </NavigationContainer>
- // );
-//};
+const RootStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Year">
+        <Stack.Screen name="Year" component={YearScreen} />
+        <Stack.Screen name="Month" component={MonthScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-//export const AppConteiner = RootStack;
+export const AppConteiner = RootStack;
